420b74233b5867740e1616b7840930199e504b68
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 15:32:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,17 @@
 # Write your code here!
+				
+
 def game_hash
   {
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
-        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
-        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
-        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
-        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>}
+        {:player_name => "Jeff Adrien", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "Bismack Biyombo", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "DeSagna Diop", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "Ben Gordon", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "Kemba Walker", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>}
       ]
     },
     :away => {
