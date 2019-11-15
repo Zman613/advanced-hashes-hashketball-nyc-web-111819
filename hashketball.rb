335b04220123f68f4cd083f6713e6aaa77880453
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 16:20:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,13 +5,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [
-        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0},
-        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0},
-        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0},
-        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0},
-        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0}
-      ]
+      :players => []
     },
     :away => {
       :team_name => "Charlotte Hornets",
@@ -25,5 +19,11 @@ def game_hash
       ]
   }
 
-  name_h = ["Alan Anderson",	"Reggie Evans",	"Brook Lopez",	"Mason Plumlee",	"Jason Terry"]
+  home = {
+    player_name: ["Alan Anderson",	"Reggie Evans",	"Brook Lopez",	"Mason Plumlee",	"Jason Terry"],
+    number: [0,	30,	11,	1, 31], shoe: [16,	14,	17,	19,	15], points: [22,	12,	17,	26,	19], rebounds: [12,	12,	19,	11,	2],
+    assists: [12,	12,	10,	6,	2], steals: [3,	12,	3,	3,	4], blocks: [1,	12,	1,	8,	11], slam_dunks: [1,	7,	15,	5,	1]
+  }
+
+
 end
