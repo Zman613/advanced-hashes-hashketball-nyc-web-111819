f107da48d8fc65dead73204c7e5639ec11c2e2de
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 15:31:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,26 @@
 # Write your code here!
 def game_hash
   {
-    :home => {:team_name => nil, :colors => [], :players => []},
-    :away => {:team_name => nil, :colors => [], :players => []}
+    :home => {
+      :team_name => "Brooklyn Nets", 
+      :colors => ["Black", "White"], 
+      :players => [
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>}
+      ]
+    },
+    :away => {
+      :team_name => "Brooklyn Nets", 
+      :colors => ["Black", "White"], 
+      :players => [
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>},
+        {:player_name => "", :number =>, :shoe =>, :points =>, :rebounds =>, :assists =>, :steals =>, :blocks =>, :slam_dunks =>}
+      ]
   }
 end
